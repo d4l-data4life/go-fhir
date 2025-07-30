6ac93fa0252909792131facd604bf9e92767b6f0
LRueckert <lars.rueckert@data4life.care>
2025-07-30 13:01:00 +0200

    add more value[x] types to Extension

--- pkg/common/types.go
+++ pkg/common/types.go
@@ -28,20 +28,57 @@ type Extension struct {
 	// identifies the meaning of the extension
 	URL string `json:"url"`
 
-	// TODO: expand with other possible types
-	// Single-valued answer to the question
-	ValueBoolean    *bool       `json:"valueBoolean,omitempty"`
-	ValueDecimal    *float64    `json:"valueDecimal,omitempty"`
-	ValueInteger    *int        `json:"valueInteger,omitempty"`
-	ValueDate       *string     `json:"valueDate,omitempty"`
-	ValueDateTime   *string     `json:"valueDateTime,omitempty"`
-	ValueTime       *string     `json:"valueTime,omitempty"`
-	ValueString     *string     `json:"valueString,omitempty"`
-	ValueUri        *string     `json:"valueUri,omitempty"`
-	ValueAttachment *Attachment `json:"valueAttachment,omitempty"`
-	ValueCoding     *Coding     `json:"valueCoding,omitempty"`
-	ValueQuantity   *Quantity   `json:"valueQuantity,omitempty"`
-	ValueReference  *Reference  `json:"valueReference,omitempty"`
+	ValueBase64Binary        *string            `json:"valueBase64Binary,omitempty"`
+	ValueBoolean             *bool              `json:"valueBoolean,omitempty"`
+	ValueCanonical           *string            `json:"valueCanonical,omitempty"`
+	ValueCode                *string            `json:"valueCode,omitempty"`
+	ValueDate                *string            `json:"valueDate,omitempty"`
+	ValueDateTime            *string            `json:"valueDateTime,omitempty"`
+	ValueDecimal             *float64           `json:"valueDecimal,omitempty"`
+	ValueId                  *string            `json:"valueId,omitempty"`
+	ValueInstant             *string            `json:"valueInstant,omitempty"`
+	ValueInteger             *int               `json:"valueInteger,omitempty"`
+	ValueInteger64           *int64             `json:"valueInteger64,omitempty"`
+	ValueMarkdown            *string            `json:"valueMarkdown,omitempty"`
+	ValueOid                 *string            `json:"valueOid,omitempty"`
+	ValuePositiveInt         *int               `json:"valuePositiveInt,omitempty"`
+	ValueString              *string            `json:"valueString,omitempty"`
+	ValueTime                *string            `json:"valueTime,omitempty"`
+	ValueUnsignedInt         *int               `json:"valueUnsignedInt,omitempty"`
+	ValueUri                 *string            `json:"valueUri,omitempty"`
+	ValueUrl                 *string            `json:"valueUrl,omitempty"`
+	ValueUuid                *string            `json:"valueUuid,omitempty"`
+	ValueAddress             *Address           `json:"valueAddress,omitempty"`
+	ValueAge                 *Age               `json:"valueAge,omitempty"`
+	ValueAnnotation          *Annotation        `json:"valueAnnotation,omitempty"`
+	ValueAttachment          *Attachment        `json:"valueAttachment,omitempty"`
+	ValueCodeableConcept     *CodeableConcept   `json:"valueCodeableConcept,omitempty"`
+	ValueCoding              *Coding            `json:"valueCoding,omitempty"`
+	ValueContactPoint        *ContactPoint      `json:"valueContactPoint,omitempty"`
+	ValueCount               *Count             `json:"valueCount,omitempty"`
+	ValueDistance            *Distance          `json:"valueDistance,omitempty"`
+	ValueDuration            *Duration          `json:"valueDuration,omitempty"`
+	ValueHumanName           *HumanName         `json:"valueHumanName,omitempty"`
+	ValueIdentifier          *Identifier        `json:"valueIdentifier,omitempty"`
+	ValueMoney               *Money             `json:"valueMoney,omitempty"`
+	ValuePeriod              *Period            `json:"valuePeriod,omitempty"`
+	ValueQuantity            *Quantity          `json:"valueQuantity,omitempty"`
+	ValueRange               *Range             `json:"valueRange,omitempty"`
+	ValueRatio               *Ratio             `json:"valueRatio,omitempty"`
+	ValueReference           *Reference         `json:"valueReference,omitempty"`
+	ValueSampledData         *interface{}       `json:"valueSampledData,omitempty"`
+	ValueSignature           *Signature         `json:"valueSignature,omitempty"`
+	ValueTiming              *Timing            `json:"valueTiming,omitempty"`
+	ValueContactDetail       *ContactDetail     `json:"valueContactDetail,omitempty"`
+	ValueContributor         *interface{}       `json:"valueContributor,omitempty"`
+	ValueDataRequirement     *interface{}       `json:"valueDataRequirement,omitempty"`
+	ValueExpression          *Expression        `json:"valueExpression,omitempty"`
+	ValueParameterDefinition *interface{}       `json:"valueParameterDefinition,omitempty"`
+	ValueRelatedArtifact     *RelatedArtifact   `json:"valueRelatedArtifact,omitempty"`
+	ValueTriggerDefinition   *TriggerDefinition `json:"valueTriggerDefinition,omitempty"`
+	ValueUsageContext        *UsageContext      `json:"valueUsageContext,omitempty"`
+	ValueDosage              *Dosage            `json:"valueDosage,omitempty"`
+	ValueMeta                *Meta              `json:"valueMeta,omitempty"`
 }
 
 // Reference is a reference from one resource to another.
